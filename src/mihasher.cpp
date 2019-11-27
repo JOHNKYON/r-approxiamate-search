74be66994ce5470374cd069620575c81d8d2d8de
JOHNKYON <zhaofengbo1321@163.com>
2019-11-27 18:56:37 +0900

    Fix bugs.

--- src/mihasher.cpp
+++ src/mihasher.cpp
@@ -82,7 +82,7 @@ void mihasher::query(UINT32 *results, UINT32* numres, qstat *stats, UINT8 *query
     int curb = b;		// current b: for the first mplus substrings it is b, for the rest it is (b-1)
 
 	printf("S is set to %d\n", S);
-    for (s = 0; s <= S && n < maxres; s++) {
+    for (s = 0; s <= S; s++) {
         for (int k=0; k<m; k++) {
             printf("s is %d and k is %d\n", s, k);
             if (k < mplus)
